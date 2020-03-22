04e9a4f42534d5225d32ea67eaa4867a461e6079
ramireddy <ramireddyhnb@gmail.com>
2020-03-21 21:33:19 -0500

    initial commit

--- src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
+++ src/main/java/com/howtodoinjava/demo/SpringBootDemoApplication.java
@@ -3,6 +3,10 @@ package com.howtodoinjava.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author VenkataRamiReddy
+ *
+ */
 @SpringBootApplication 
 public class SpringBootDemoApplication {
 
